9acde7eddc41436578080bed2eb78d4b7ebcd560
ATC <alvaro@teje.ro>
2024-05-02 15:14:35 +0200

    Unpin channels to a specific commit.

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -2,8 +2,6 @@
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
-        (commit
-          "541b1c3e74cbb7965b7398d342882a038c893fbb")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -13,8 +11,6 @@
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
-        (commit
-          "639c25bb691129607b4aa5bd87ad13f9c3d2667c")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
